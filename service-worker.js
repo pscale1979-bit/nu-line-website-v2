b4a166b3f476f0317f247396ca1a0a78c934fac7
pscale1979-bit <pscale1979@gmail.com>
2026-08-31 12:27:05 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="nu-line-edition-v3.4";
-const CORE=["./","index.html","styles.css?v=3.4.0","app.js?v=3.4.0","config.js?v=3.4.0","pages.json","manifest.webmanifest","01-cover.jpg"];
+const CACHE="nu-line-edition-v3.5";
+const CORE=["./","index.html","hansen-steel-look-aluminium-scotland.html","hansen.css?v=3.5.0","styles.css?v=3.5.0","app.js?v=3.5.0","config.js?v=3.5.0","pages.json","manifest.webmanifest","01-cover.jpg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
